f471057aee6a44bc5963e6a447f5fee428325e57
kniazik <knyazik.d@gmail.com>
2024-03-18 16:49:42 +0200

    Format code

--- Dark Chat/AppDelegate.swift
+++ Dark Chat/AppDelegate.swift
@@ -13,7 +13,8 @@ import Firebase
 final class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, 
+                     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
         
         UIApplication.shared.statusBarStyle = .lightContent
